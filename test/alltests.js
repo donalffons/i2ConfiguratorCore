0ffc700a68305ecb3e3d716d1630062c4ae7d77d
Sebastian <donalffons@gmail.com>
2018-08-15 18:55:04 +0200

    some testing improvements

--- test/alltests.js
+++ test/alltests.js
@@ -26,13 +26,25 @@ exports.allTests = async function(test){
                 if(msg.text() == "---TEST FINISHED---") {
                     resolve();
                 }
+                if(msg.type() == "error") {
+                    test.ok(false);
+                }
             });
         });
     }
+    
+    await console.log("---Starting Test: testphpinfo---\n");
+    await page.goto('http://localhost/test/testphpinfo.php', {waitUntil: 'load'});
+    let bodyHTML = await page.evaluate(() => document.body.innerHTML);
+    console.log(bodyHTML);
+    await console.log("---Finished Test: testphpinfo---\n");
+
+    await console.log("---Starting Test: test01---\n");
     await page.goto('http://localhost/test/test01.html', {waitUntil: 'load'});
     await testFinished();
+    await console.log("---Finished Test: test01---\n");
 
-    await console.log("-----Finish Testing-----\n");
+    await console.log("---Finished all tests---\n");
     await browser.close();
     test.done();
 };
--- test/test01.js
+++ test/test01.js
@@ -1,14 +1,17 @@
 "use strict";
 
-import i2Model from "../src/i2Model.js";
 import i2ModelBuilder from "../src/i2ModelBuilder.js";
 
+console.log("creating new model");
 i2ModelBuilder.createNewModel(function(testmodel) {
     testmodel.setName("Hallo Welt");
+    console.log("model: " + JSON.stringify(testmodel));
+    console.log("saving model");
     testmodel.save(function(){
-        console.log(JSON.stringify(testmodel));
+        console.log("model: " + JSON.stringify(testmodel));
+        console.log("deleting model");
         testmodel.delete(function(){
-            console.log(JSON.stringify(testmodel));
+            console.log("model: " + JSON.stringify(testmodel));
             console.log("---TEST FINISHED---")
         });
     });
