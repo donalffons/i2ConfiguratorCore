c1c24c452a211678821ef8b6b4b3020f40556f32
Sebastian <donalffons@gmail.com>
2018-08-16 14:43:58 +0200

    Building using uglifyjs using grunt build

--- gruntfile.js
+++ gruntfile.js
@@ -2,10 +2,25 @@ module.exports = function(grunt) {
   grunt.initConfig({
     nodeunit: {
       all: ['test/alltests.js']
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'build/i2ConfiguratorCore.min.js': ['src/i2Action.js', 'src/i2Database.js']
+        }
+      },
+      options: {
+        mangle: {
+        },
+        compress: {
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
 
   grunt.registerTask('default', ['nodeunit']);
+  grunt.registerTask('build', ['uglify']);
 };
