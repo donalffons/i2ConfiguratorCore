e7c2b5151656b545ea6a37f1f37b5d99b71e758d
Sebastian <donalffons@gmail.com>
2018-08-16 11:56:51 +0200

    remove old grunt-php code

--- gruntfile.js
+++ gruntfile.js
@@ -2,19 +2,10 @@ module.exports = function(grunt) {
   grunt.initConfig({
     nodeunit: {
       all: ['test/alltests.js']
-    },
-    php: {
-        test: {
-            options: {
-                port: 3000,
-                keepalive: false
-            }
-        }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  //grunt.loadNpmTasks('grunt-php');
 
-  grunt.registerTask('default', [/*'php:test', */'nodeunit']);
+  grunt.registerTask('default', ['nodeunit']);
 };
