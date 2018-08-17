1b247cad8fdc6e862994b6a796ef3ff98b7293ad
Sebastian <donalffons@gmail.com>
2018-08-17 12:15:43 +0200

    Fixed i2ActionObjectProperty

--- src/i2ActionObjectProperty.js
+++ src/i2ActionObjectProperty.js
@@ -4,27 +4,25 @@ class i2ActionObjectProperty extends i2Action {
     constructor() {
         super();
         setType("i2ActionObjectProperty");
-        delete this.data.action;
         this.objectsSelector = null;
         this.property = null;
         this.value = null;
     }
-    
-    setObjectsSelector(sel) { this.objectsSelector = sel; }
-    getObjectsSelector() { return this.objectsSelector; }
 
-    setProperty(prop) { this.property = prop; }
-    getProperty() { return this.property; }
-
-    setValue(value) { this.value = value; }
-    getValue() { return this.value; }
-
-    getAction() {
-        return JSON.stringify({
+    updateAction() {
+        this.data.action = JSON.stringify({
             objectSelector: this.objectsSelector,
             property: this.property,
             value: this.value
-        })
+        });
     }
-    setAction() { console.error("Cannot call setAction!"); }
+    
+    setObjectsSelector(sel) { this.objectsSelector = sel; updateAction(); }
+    getObjectsSelector() { return this.objectsSelector; }
+
+    setProperty(prop) { this.property = prop; updateAction(); }
+    getProperty() { return this.property; }
+
+    setValue(value) { this.value = value; updateAction(); }
+    getValue() { return this.value; }
 }
