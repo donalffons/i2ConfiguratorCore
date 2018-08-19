fe3bd93a2c44f5e2362aa947047f170caf5df4a6
Sebastian <donalffons@gmail.com>
2018-08-19 17:20:35 +0200

    some more logging for test07

--- test/test07.js
+++ test/test07.js
@@ -4,18 +4,23 @@
 // create new model, add tag, remove tag and delete model
 
 async function test() {
+    console.log("Creating model");
     var model = await i2ModelBuilder.createNewModel();
     await model.save();
 
+    console.log("adding a tag");
     model.addTag("sometag", "somevalue");
+    console.log("saving model");
     await model.save();
     if(model.getTags()["sometag"] != "somevalue") {
         console.error("tag not correctly added to model!");
     }
+    console.log("removing the tag");
     model.removeTag("sometag");
     if(Object.keys(model.getTags()).length != 0) {
         console.error("tag not correct removed from model!");
     }
+    console.log("saving again");
     await model.save();
     model.delete();
     console.log("---TEST FINISHED---")
