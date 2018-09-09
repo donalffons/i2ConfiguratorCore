36bfce518b6d813922efd35807888ad45cabafdf
Sebastian <donalffons@gmail.com>
2018-09-09 15:20:46 +0200

    moving overrides from material.overrides to material.userData.overrides, handling in action

--- src/i2ActionObjectProperty.js
+++ src/i2ActionObjectProperty.js
@@ -37,11 +37,47 @@ class i2ActionObjectProperty extends i2Action {
     setValue(value) { this.value = value; }
     getValue() { return this.value; }
 
+    initialize(data) {
+        if(data && data.objectsSelector) {
+            this.setObjectsSelector(data.objectsSelector);
+        }
+        if(data && data.property) {
+            this.setProperty(data.property);
+        }
+        if(data && data.value) {
+            this.setValue(data.value);
+        }
+        if(data && data.tags) {
+            this.setTags(data.tags);
+        }
+
+        let objects = this.getObjectsSelector().getObjects();
+        objects.forEach((o) => {
+            if(o.userData.overrides === undefined) {
+                o.userData.overrides = {};
+            }
+            if(o.userData.overrides[this.getProperty()] !== undefined) {
+                console.error("another override for this property already exists!");
+            }
+            o.userData.overrides[this.getProperty()] = {};
+            o.userData.overrides[this.getProperty()].overridden = false;
+            o.userData.overrides[this.getProperty()].default = (data && data.default) ? data.default : Object.assign({}, o[this.getProperty()]); // shallow clone
+        });
+    }
     execute() {
         let objects = this.objectsSelector.getObjects();
         objects.forEach((object) => {
             let value = this.value.getValueData();
             eval("object."+this.property+".copy(value)");
+            object.userData.overrides[this.getProperty()].overridden = true;
+        });
+    }
+    revert() {
+        let objects = this.objectsSelector.getObjects();
+        objects.forEach((object) => {
+            let value = object.userData.overrides[this.getProperty()].default;
+            eval("object."+this.property+".copy(value)");
+            object.userData.overrides[this.getProperty()].overridden = false;
         });
     }
 }
